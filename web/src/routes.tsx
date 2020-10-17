b1321281ce8df004f133526b3d5b1ac520f2cc82
Luiz Carlos Abbott Galvao Neto <luiz.n@adireto.com>
2020-10-17 16:32:06 -0300

    feat: 🎸 Finish map page

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
 import OrphanagesMaps from "./pages/OrphanagesMap";
+import Orphanage from "./pages/Orphanage";
+import CreateOrphanage from "./pages/CreateOrphanage";
 
 function Routes() {
   return (
@@ -9,6 +11,8 @@ function Routes() {
       <Switch>
         <Route path="/" component={Landing} exact />
         <Route path="/app" component={OrphanagesMaps} />
+        <Route path="/orphanages/create" component={CreateOrphanage} />
+        <Route path="/orphanages/:id" component={Orphanage} />
       </Switch>
     </BrowserRouter>
   );
